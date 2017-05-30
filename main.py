9338c4ba45658974b71bece98f25933cbd0cc6ab
Sunyouh <sunyouh35@gmail.com>
2017-05-30 20:38:58 +0900

    add car_racing file, which is our target env.

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ for i in range(1000):
     observ = env.reset()
     observ_samples.append(observ)
 
-    print(env.action_space.n)
+    print(env.action_space)
 
     done = False
     while not done:
-        action = np.random.randint(0, env.action_space)
-        observ, reward, done, _ = env.step(action)
+        # action = np.random.randint(0, env.action_space)
+        observ, reward, done, _ = env.step(5)
         observ_samples.append(observ)
 
 observ_samples = np.array(observ_samples)
