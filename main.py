c4bb1ee206c937883ae8eadb5d629e6fe7b5fd9f
Sunyouh <sunyouh35@gmail.com>
2017-05-30 21:45:31 +0900

    converting observed state rgb to gray.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import gym
+import cv2
 
 env = gym.make('CarRacing-v0')
 
@@ -9,14 +10,28 @@ for i in range(1000):
     observ = env.reset()
     observ_samples.append(observ)
 
-    print(env.action_space)
+    # print(env.action_space)
+    np.set_printoptions(threshold='nan')
 
     done = False
     while not done:
-        # action = np.random.randint(0, env.action_space)
-        observ, reward, done, _ = env.step(5)
-        observ_samples.append(observ)
+        # Step fn input; steer, gas, brake
 
-observ_samples = np.array(observ_samples)
+        observ, reward, done, _ = env.step(env.action_space.sample())
+        # observ_samples.append(observ)
 
-env = gym.wrappers.Monitor(env, 'monitor-folder', force=True)
+        env.render()
+
+        # convert observ rgb to grayscale
+        cvted = cv2.cvtColor(observ, cv2.COLOR_RGB2GRAY)
+
+        print(cvted.shape, reward)
+
+        # show grayscale-converted state
+        cv2.imshow('img', cvted)
+        cv2.waitKey(1)
+
+
+# observ_samples = np.array(observ_samples)
+#
+# env = gym.wrappers.Monitor(env, 'monitor-folder', force=True)
